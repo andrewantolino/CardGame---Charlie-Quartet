0dac5699b01d335c6557d2a01c5f6e8e653fadd3
gayatri <gayatri@LAPTOP-OLEGNKSG>
2017-03-23 19:58:39 +1100

    eddited deck and card class

--- CharlieQuartet/CharlieQuartet/Card.cs
+++ CharlieQuartet/CharlieQuartet/Card.cs
@@ -8,7 +8,31 @@ namespace CharlieQuartet
 {
     public class Card
     {
+        private string number;
+        public Suit _suit;
+        public int point;
+
+        public Card(Suit suit, Rank rank)
+        {
+            _suit = suit;
+        }
+
+
+        public string getNumber()
+        {
+            return number;
+        }
+
+        public void setNumber(string aNumber)
+        {
+            number = aNumber;
+        }
+
+
+       
         
+
+        }
 
     }
 }
--- CharlieQuartet/CharlieQuartet/Deck.cs
+++ CharlieQuartet/CharlieQuartet/Deck.cs
@@ -9,6 +9,42 @@ namespace CharlieQuartet
     public class Deck
     {
 
+        private List<Card> _cards;
+
+        public Deck(string pnumber, string psuit, int ppoint)
+        {
+            Suit[] mySuits = new Suit[] {Suit.Spades,Suit.Hearts,Suit.Clubs,Suit.Diamonds };
+            Rank[] myRank = new Rank[] { Rank.Ace, Rank.Two, Rank.Three, Rank.Four, Rank.Five, Rank.Six, Rank.Seven, Rank.Eight, Rank.Nine, Rank.Jack, Rank.Queen, Rank.king };
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 13; j++)
+                {
+                    cards.Add(new Card(mySuits[i], myRank[j]));
+                    
+                }
+            }
+            _cards = new List<Card>();
+            
+           
+            Console.WriteLine();
+        }
+
+        public void Shuffle()
+        {
+            Random rand = new Random();
+            int r = rand.Next(0, 52);
+
+            Card[] cards = new Card[] { };
+          
+            for (int i = 0; i < 52; i++)
+            {
+                if (Equals(cards[i], _cards[r] ))
+                {
+
+                }
+               //ccards[i] = 
+            }
+        }
 
     }
 }
